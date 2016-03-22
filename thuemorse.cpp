cf774c2cb0b6ac6b0dc2249126e9f653b150b097
Pat Morin <morin@scs.carleton.ca>
2016-03-22 09:26:36 -0400

    Small changes

--- thuemorse.cpp
+++ thuemorse.cpp
@@ -1,19 +1,28 @@
+/*
+    gcc -Wall -std=c++11 thuemorse.cpp -o thuemorse
+ */
 #include<iostream>
 
-int main(int argc, char *argv[]) {
-    std::uint32_t n = 1000, i = 0;
-   
-    int gap = 0; 
-    while (i < n) {
-        if (__builtin_parity(i)) {
-            gap++;
-        } else {
-            // std::cout << gap;
-            gap = 0;
-        }
+// Print the first n bits of the Prouhet-Thue-Morse string
+void thue_morse(std::uint32_t n) {
+    for (std::uint32_t i = 0; i < n; i++)
         std::cout << __builtin_parity(i);
-        i++;
+    std::cout << std::endl;
+}
+
+// Print the first n characters of Thue's non-repetitive string
+void non_repetitive(std::uint32_t n) {
+    std::uint32_t i = 1;
+    for (std::uint32_t j = 0; j < n; j++) {
+        int gap = 0;
+        while (__builtin_parity(i++)) gap++;
+        std::cout << gap;
     }
     std::cout << std::endl;
-    return 0;
+}
+
+int main(int argc, char *argv[]) {
+   std::uint32_t n = 100;
+   thue_morse(n);
+   non_repetitive(n);
 }
